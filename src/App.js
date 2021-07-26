05d32a1c146e0a0a2ca3df3d3ca889117384c5ac
narishin <nariri0814@gmail.com>
2021-07-26 17:14:12 +0900

    7.26

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+// import { HashRouter, Route } from 'react-router-dom';
 import Home from './layout/Home';
-import Overseas from './coronaApi/Overseas';
+// import Overseas from './coronaApi/Overseas';
 import './App.css';
 
 const App = () => {
     return (
-      <HashRouter>
-        <Route path="/" exact={true} component={Home}/>
-        <Route path="/overseas" component={Overseas}/>
-      </HashRouter>
+      // <HashRouter>
+      //   <Route path="/" exact={true} component={Home}/>
+      //   <Route path="/overseas" component={Overseas}/>
+      // </HashRouter>
+      <Home/>
     )
 }
 
--- src/layout/Home.js
+++ src/layout/Home.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import CoronaState from '../coronaApi/CoronaState';
 import Graph from '../coronaApi/Graph';
